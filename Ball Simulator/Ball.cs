24b2d30b834dea1ade85199b49337f39b2b75812
Austin Baccus <gunrak117@live.com>
2018-06-03 11:19:16 -0700

    Cleaned up project formatting.

---
+++ Ball Simulator/Ball.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Ball_Simulator
+{
+    public class Ball
+    {
+        #region Fields
+
+        public double mass;
+        public double radius;
+
+        // coordinates
+        public double x;
+        public double y;
+
+        // velocity
+        public double velocity_x;
+        public double velocity_y;
+        
+        // bounciness
+        public double elasticity;
+        public double coefficientOfRestitution;
+
+        // gravity
+        public double gravity;
+
+        // states
+        public bool isAtRest;
+        public bool isRolling;
+
+        // friction
+        public double coefficientOfRollingFriction;
+
+        #endregion
+
+        #region Constructors
+
+        public Ball(
+            double mass,
+            double radius,
+            double initial_x,
+            double initial_y,
+            double initialVelocity_x,
+            double initialVelocity_y,
+            double elasticity,
+            double restitution,
+            double gravity,
+            double rollingFriction)
+        {
+            this.mass = mass;
+            this.radius = radius;
+            this.x = initial_x;
+            this.y = initial_y;
+            this.velocity_x = initialVelocity_x;
+            this.velocity_y = initialVelocity_y;
+            this.elasticity = elasticity;
+            this.coefficientOfRestitution = restitution;
+            this.gravity = gravity;
+            this.coefficientOfRollingFriction = rollingFriction;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Move(double timeIncrement, double boundWidth, double boundHeight)
+        {
+            velocity_x += 0;
+            velocity_y += gravity * timeIncrement;
+
+
+            if (WillBeOutsideXBound(timeIncrement, boundWidth) &&
+                WillBeOutsideYBound(timeIncrement, boundHeight))
+            {
+                BounceX(timeIncrement, boundWidth);
+                BounceY(timeIncrement, boundHeight);
+            }
+            else if (WillBeOutsideXBound(timeIncrement, boundWidth) &&
+                    !WillBeOutsideYBound(timeIncrement, boundHeight))
+            {
+                y += velocity_y * timeIncrement;
+                BounceX(timeIncrement, boundWidth);
+            }
+            else if (WillBeOutsideYBound(timeIncrement, boundHeight) &&
+                    !WillBeOutsideXBound(timeIncrement, boundWidth))
+            {
+                x += velocity_x * timeIncrement;
+                BounceY(timeIncrement, boundHeight);
+            }
+            else
+            {
+                x += velocity_x * timeIncrement;
+                y += velocity_y * timeIncrement;
+            }
+
+            if (IsBallRolling())
+            {
+                AdjustForFriction(timeIncrement);
+            }
+        }
+
+        public void BounceX(double timeIncrement, double boundWidth)
+        {
+            double energyLostToElasticCompression = 1;
+            double postBounceVelocity_x = 0;
+
+            #region Hits left wall
+            if (velocity_x < 0)
+            {
+                postBounceVelocity_x =
+                    Math.Sqrt(
+                        Math.Pow(coefficientOfRestitution, 2) * Math.Pow(velocity_x, 2));
+                
+                velocity_x = Math.Max(postBounceVelocity_x - energyLostToElasticCompression, 0);
+            }
+            #endregion
+
+            #region Hits right wall
+            else if (velocity_x > 0)
+            {
+                postBounceVelocity_x =
+                    -Math.Sqrt(
+                        Math.Pow(coefficientOfRestitution, 2) * Math.Pow(velocity_x, 2));
+
+                velocity_x = Math.Min(postBounceVelocity_x + energyLostToElasticCompression, 0);
+            }
+            #endregion
+        }
+
+        public void BounceY(double timeIncrement, double boundHeight)
+        {
+            double energyLostToElasticCompression = 1;
+            
+            double postBounceVelocity_y =
+                Math.Sqrt(
+                    Math.Pow(coefficientOfRestitution, 2) * Math.Pow(velocity_y, 2));
+            
+            velocity_y = Math.Max(postBounceVelocity_y - energyLostToElasticCompression, 0);
+        }
+        
+        public bool WillBeOutsideXBound(double timeIncrement, double boundWidth)
+        {
+            double d_x = velocity_x * timeIncrement;
+
+            double newXCoord = x + d_x;
+
+            double boundLeft = 0;
+            double boundRight = boundWidth;
+
+            if (newXCoord + radius > boundRight || newXCoord + 0 < boundLeft)
+                return true;
+
+            return false;
+        }
+
+        public bool WillBeOutsideYBound(double timeIncrement, double boundHeight)
+        {
+            double d_y = velocity_y * timeIncrement;
+            
+            double newYCoord = y + d_y;
+            
+            double boundFloor = 0;
+            
+            if (newYCoord - radius < boundFloor)
+                return true;
+
+            return false;
+        }
+
+        private bool IsBallRolling()
+        {
+            // if y acceleration = 0
+            // and if y = 0
+            // and if x velocity != 0
+            // return true
+
+            if (velocity_x != 0 && 
+                y < radius + (radius * 0.2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void AdjustForFriction(double timeIncrement)
+        {
+            if (velocity_x > 0)
+                velocity_x = Math.Max(0, -(coefficientOfRollingFriction * gravity * -timeIncrement) + velocity_x);
+            if (velocity_x < 0)
+                velocity_x = Math.Min(0, (coefficientOfRollingFriction * gravity * -timeIncrement) + velocity_x);
+        }
+
+        #endregion
+    }
+}
--- Ball Simulator/MainWindow.xaml.cs
+++ Ball Simulator/MainWindow.xaml.cs
@@ -12,17 +12,172 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.ComponentModel;
+using System.Threading;
+
 
 namespace Ball_Simulator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Ellipse myEllipse = new Ellipse();
+        Ball ball;
+        double time;
+        double timeIncrement = .05;
+        double[] centerCoord = new double[2];
+
+        double ballAreaWidth = 700;
+        double ballAreaHeight = 300;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            centerCoord[0] = ballAreaWidth / 2;
+            centerCoord[1] = ballAreaHeight / 2;
+
+            ball = new Ball(1, 10, centerCoord[0], centerCoord[1], 0, 0, .85, 0.65, -9.81, 0.00);
+
+            myEllipse.Stroke = Brushes.Black;
+            myEllipse.Fill = Brushes.DarkBlue;
+            myEllipse.HorizontalAlignment = HorizontalAlignment.Center;
+            myEllipse.VerticalAlignment = VerticalAlignment.Center;
+            myEllipse.Width = ball.radius;
+            myEllipse.Height = ball.radius;
+
+            mainCanvas.Children.Add(myEllipse);
+
+            Canvas.SetLeft(myEllipse, ball.x);
+            Canvas.SetTop(myEllipse, ball.y);
+
+            txt_x.Text = "x:\t" + RoundDecimals(ball.x, 2);
+            txt_y.Text = "y:\t" + RoundDecimals(ball.y, 2);
+        }
+
+        #region Pointers
+
+        private async void btn_beginSimulation_Click(object sender, RoutedEventArgs e)
+        {
+            await BeginSimulation();
+        }
+
+        private void btn_restartSimulation_Click(object sender, RoutedEventArgs e)
+        {
+            RestartSimulation();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Asynchronously starts the simulation.
+        /// </summary>
+        /// <returns></returns>
+        async Task BeginSimulation()
+        {
+            btn_beginSimulation.IsEnabled = false;
+            ball = new Ball(
+                1,
+                Double.Parse(txt_radius.Text),
+                centerCoord[0],
+                centerCoord[1],
+                Double.Parse(txt_vX.Text),
+                Double.Parse(txt_vY.Text),
+                0.85,
+                Double.Parse(txt_restitution.Text),
+                Double.Parse(txt_gravity.Text),
+                Double.Parse(txt_friction.Text));
+
+            await Task.Run(async () =>
+            {
+                while (!ball.isAtRest)
+                {
+                    await Task.Delay(1);
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        UpdateUI();
+                        DrawTrail();
+                    }), null);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Restarts the simulation and pauses the movement of the ball.
+        /// </summary>
+        void RestartSimulation()
+        {
+            btn_beginSimulation.IsEnabled = true;
+            ball.isAtRest = true;
+
+            ball = new Ball(1, 10, centerCoord[0], centerCoord[1], 0, 0, .85, 0.65, 0, 0.00);
+            Canvas.SetLeft(myEllipse, ball.x);
+            Canvas.SetTop(myEllipse, ball.y);
+        }
+
+        void UpdateUI()
+        {
+            //Update the time and ball position
+            time += timeIncrement;
+            ball.Move(timeIncrement, ballAreaWidth, ballAreaHeight);
+            
+            //Draw the ball
+            Canvas.SetLeft(myEllipse, ball.x);
+            Canvas.SetTop(myEllipse, ballAreaHeight - ball.y);
+            
+            //Update the text in the corner of the screen that displays the coordinates of the ball
+            txt_x.Text = "x:\t" + RoundDecimals(ball.x, 2);
+            txt_y.Text = "y:\t" + RoundDecimals(ball.y, 2);
+        }
+
+        /// <summary>
+        /// Draws te breadcrumb trail of the ball as it moves.
+        /// </summary>
+        void DrawTrail()
+        {
+            Ellipse p = new Ellipse();
+            p.Stroke = Brushes.Black;
+            p.Fill = Brushes.DarkBlue;
+            p.HorizontalAlignment = HorizontalAlignment.Center;
+            p.VerticalAlignment = VerticalAlignment.Center;
+            p.Width = ball.radius * 0.1;
+            p.Height = ball.radius * 0.1;
+
+            mainCanvas.Children.Add(p);
+
+            Canvas.SetLeft(p, ball.x + ball.radius / 2);
+            Canvas.SetTop(p, ballAreaHeight - ball.y + ball.radius / 2);
+        }
+
+        /// <summary>
+        /// Rounds lengthy decimal numbers that are outputted to the window.
+        /// </summary>
+        /// <param name="num">The number to round.</param>
+        /// <param name="rnd">The number decimal places to round to.</param>
+        /// <returns></returns>
+        string RoundDecimals(double num, int rnd)
+        {
+            string newNum = ""; 
+            if ((num + "").Contains("."))
+            {
+                int lengthOfNonDecimalString = 0;
+                for (int i = 0; i < (num + "").IndexOf('.'); i++)
+                {
+                    newNum += (num + "").ElementAt(i);
+                    lengthOfNonDecimalString++;
+                }
+
+                if (rnd > 0) newNum += ".";
+                lengthOfNonDecimalString++;
+
+                for (int i = (num + "").IndexOf('.') + 1; i < Math.Min(rnd + lengthOfNonDecimalString, (num + "").Length - (num + "").IndexOf('.')); i++)
+                {
+                    newNum += (num + "").ElementAt(i);
+                }
+
+                return newNum;
+            }
+            else return num + "";
         }
     }
 }
